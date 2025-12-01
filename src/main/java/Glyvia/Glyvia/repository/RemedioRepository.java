e0e687d6049b1e0192d14ceadc9e7bfb87ecc8c2
Aline Hana <alineshinkawa@hotmail.comgif config --global user.email alineshinkawa@hotmail.comgit config --global user.email alineshinkawa@hotmail.comgit commit -m Project>
2025-12-01 17:55:24 -0300

    .

--- src/main/java/Glyvia/Glyvia/repository/RemedioRepository.java
+++ src/main/java/Glyvia/Glyvia/repository/RemedioRepository.java
@@ -1,6 +1,5 @@
 package Glyvia.Glyvia.repository;
 
-import Glyvia.Glyvia.model.Consulta;
 import Glyvia.Glyvia.model.Remedio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,9 +8,7 @@ import java.util.List;
 public interface RemedioRepository extends JpaRepository<Remedio, Long> {
     List<Remedio> findByUsuario_Id(Long usuarioId);
     
-    List<Consulta> findByConsultaUsuarioId(Long idUsuario);
 
-    List<Remedio> findByRemedioUsuarioId(Long idUsuario);
 
 
 }
